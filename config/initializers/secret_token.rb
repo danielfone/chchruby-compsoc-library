c6b7121a71b503d1c717e505ac1906cbb329a85f
Daniel Fone <daniel@fone.net.nz>
2014-07-03 20:36:02 +1200

    Rails 3.2.19

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CompsocLibrary::Application.config.secret_token = '2f8feb0b4976cdfbafc27596bc2a4b93664df791f69dc32977c1e09e00b98e4a5ede9b7d49dc052044f8ba74c1faee9e12ff7ce4c2e684f061c480fcbfa8179a'
