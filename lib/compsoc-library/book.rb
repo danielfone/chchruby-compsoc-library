9eea854068fb97850da0e7806e8b80ba57d01ac1
Daniel Fone <daniel@fone.net.nz>
2014-07-05 13:15:31 +1200

    Comment re Idiom

--- lib/compsoc-library/book.rb
+++ lib/compsoc-library/book.rb
@@ -4,6 +4,13 @@ module CompsocLibrary
     attr_accessor :due_on
 
     def on_loan?
+      # !! is a ruby idiom that converts a 'truthy' value to true.
+      # It simply applies the negation operator (!) twice.
+      #
+      # In this case, borrower can either be a Borrower object or nil
+      # If borrower is set:
+      #   !!borrower => !(!<#Borrower>) => !(false) => true
+      #   !!borrower => !(!nil)         => !(true)  => false
       !!borrower
     end
 
